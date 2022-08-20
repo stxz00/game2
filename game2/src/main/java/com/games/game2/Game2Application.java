3393f52257f933948970164cc7ac17fdbef51e16
Hanam Lee <77870749+stxz00@users.noreply.github.com>
2022-08-20 21:27:09 +0900

    Initial commit

---
+++ game2/src/main/java/com/games/game2/Game2Application.java
@@ -0,0 +1,13 @@
+package com.games.game2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Game2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Game2Application.class, args);
+	}
+
+}
